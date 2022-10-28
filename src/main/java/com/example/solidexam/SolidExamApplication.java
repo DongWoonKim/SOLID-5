d1620f4fb4df0e3a6a4ef2c4c0143f4893ff27ae
DongWoonKim <01030985843z@naver.com>
2022-10-28 13:27:53 +0900

    first setup

---
+++ src/main/java/com/example/solidexam/SolidExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.solidexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolidExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/solidexam/SolidExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solidexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
